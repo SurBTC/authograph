009110bc9646da8715aee6fd864a28702139371f
Ignacio Baixas <iobaixas@gmail.com>
2017-07-16 23:32:27 -0400

    chore(): removes scaffolding spec

--- spec/authograph_spec.rb
+++ spec/authograph_spec.rb
@@ -4,8 +4,4 @@ describe Authograph do
   it 'has a version number' do
     expect(Authograph::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
